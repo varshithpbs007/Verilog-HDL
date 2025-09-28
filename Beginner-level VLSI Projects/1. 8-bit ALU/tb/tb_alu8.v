892caae564bd5cce4151e877165736569bc61dd3
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-09-28 10:45:05 +0530

    Update tb_alu8.v

--- Beginner-level VLSI Projects/1. 8-bit ALU/tb/tb_alu8.v
+++ Beginner-level VLSI Projects/1. 8-bit ALU/tb/tb_alu8.v
@@ -10,28 +10,27 @@ module tb_alu8;
 
     initial begin
         $dumpfile("alu8.vcd");
-        $dumpvars(0, tb_alu8);
-        A = 8'h00;
-        B = 8'h00;
-        op = 3'b000;
+        $dumpvars(0);
+        //A = 8'h00;
+        //B = 8'h00;
+        //op = 3'b000;
 
         $display("time\tA\tB\top\tY\tZ C V");
         $monitor("%0t\t%h\t%h\t%b\t%h\t%b %b %b",$time,A,B,op,Y,Z,C,V);
 
         // Stimulus
-        #5 A = 8'h00; B = 8'h00; op = 3'b000; #10; // add 0+0
-        A = 8'h05; B = 8'h03; op = 3'b000; #10;    // add 5+3
-        A = 8'hFF; B = 8'h01; op = 3'b000; #10;    // add with carry (255+1)
-        A = 8'h05; B = 8'h03; op = 3'b001; #10;    // sub 5-3
-        A = 8'h00; B = 8'h01; op = 3'b001; #10;    // sub borrow 0-1
-        A = 8'hF0; B = 8'h0F; op = 3'b010; #10;    // AND
-        A = 8'hF0; B = 8'h0F; op = 3'b011; #10;    // OR
-        A = 8'hFF; B = 8'h0F; op = 3'b100; #10;    // XOR
-        A = 8'h80; op = 3'b101; #10;               // SHL (msb out)
-        A = 8'h01; op = 3'b110; #10;               // SHR (lsb out)
-        A = 8'hAA; op = 3'b111; #10;               // PASS A
-        #10;
+        #50 A = 8'h00; B = 8'h00; op = 3'b000; #100; // add 0+0
+        A = 8'h05; B = 8'h03; op = 3'b000; #100;    // add 5+3
+        A = 8'hFF; B = 8'h01; op = 3'b000; #100;    // add with carry (255+1)
+        A = 8'h05; B = 8'h03; op = 3'b001; #100;    // sub 5-3
+        A = 8'h00; B = 8'h01; op = 3'b001; #100;    // sub borrow 0-1
+        A = 8'hF0; B = 8'h0F; op = 3'b010; #100;    // AND
+        A = 8'hF0; B = 8'h0F; op = 3'b011; #100;    // OR
+        A = 8'hFF; B = 8'h0F; op = 3'b100; #100;    // XOR
+        A = 8'h80; op = 3'b101; #100;               // SHL (msb out)
+        A = 8'h01; op = 3'b110; #100;               // SHR (lsb out)
+        A = 8'hAA; op = 3'b111; #100;               // PASS A
+        #100;
         $finish;
     end
 endmodule
-
